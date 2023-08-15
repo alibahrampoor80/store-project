e102d2e758b0aeca80c4091eafc6f8d795943159
ali bahrampoor <ali.bahrampoor1380@gmail.com>
2023-08-15 16:37:32 +0330

    create section remove product and fixed bug add product and add string to array colors product for create product

--- app/router/admin/product.js
+++ app/router/admin/product.js
@@ -5,7 +5,7 @@ const router = require('express').Router()
 
 
 router.post("/add", uploadFile.array('images', 10),
-    stringToArray('tags'),  stringToArray('colors'), ProductController.addProduct)
+    stringToArray('tags'), stringToArray('colors'), ProductController.addProduct)
 
 router.get('/list', ProductController.getAllProduct)
 router.get('/:id', ProductController.getOneProduct)
